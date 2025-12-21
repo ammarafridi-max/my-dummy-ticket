c5738e1af78c322d4e996cec9b06fcf33029f7bd
Ammar Afridi <ammar.afridi95@gmail.com>
2025-12-21 16:33:19 +0400

    BaseURL renamed to BACKEND

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASEURL = import.meta.env.VITE_BACKEND_URL;
+export const BACKEND = import.meta.env.VITE_BACKEND_URL;
 export const VIEWTRIP_URL = import.meta.env.VITE_VIEWTRIP_BACKEND;
