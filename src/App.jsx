d6694a559cb118704a04079fa597ced4e1f9cb83
Ammar Afridi <ammar.afridi95@gmail.com>
2025-12-24 14:29:45 +0400

    New AppRoute file created

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,10 @@ import '@fontsource-variable/nunito';
 import '@fontsource/merriweather';
 import { useEffect } from 'react';
 import { initializeGA } from './utils/analytics';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer, Zoom } from 'react-toastify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import ScrollToTop from './components/ScrollToTop';
-
-// Layout
-import Layout from './components/Layout';
-import BookingLayout from './components/BookingLayout';
-
-// Pages
-import Home from './pages/Home';
-import FlightReservation from './pages/FlightReservation';
-import DummyTicketForSchengenVisa from './pages/DummyTicketForSchengenVisa';
-import OnwardTicket from './pages/OnwardTicket';
-import PageNotFound from './pages/PageNotFound';
-import FAQ from './pages/FAQ';
-import SelectFlights from './pages/SelectFlights';
-import ReviewDetails from './pages/ReviewDetails';
-import PaymentSuccess from './pages/PaymentSuccess';
-import TermsAndConditions from './pages/TermsAndConditions';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import Sitemap from './pages/Sitemap';
-import BlogPost from './pages/BlogPost';
-import Blog from './pages/Blog';
+import AppRoutes from './app/AppRoutes';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -45,29 +24,7 @@ function App() {
     <HelmetProvider>
       <ToastContainer transition={Zoom} />
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <ScrollToTop />
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="dummy-ticket-schengen-visa" element={<DummyTicketForSchengenVisa />} />
-              <Route path="onward-ticket" element={<OnwardTicket />} />
-              <Route path="flight-reservation" element={<FlightReservation />} />
-              <Route path="*" element={<PageNotFound />} />
-              <Route path="sitemap" element={<Sitemap />} />
-              <Route path="faq" element={<FAQ />} />
-              <Route path="payment-successful" element={<PaymentSuccess />} />
-              <Route path="terms-and-conditions" element={<TermsAndConditions />} />
-              <Route path="privacy-policy" element={<PrivacyPolicy />} />
-              <Route path="blog" element={<Blog />} />
-              <Route path="blog/:slug" element={<BlogPost />} />
-              <Route path="booking" element={<BookingLayout />}>
-                <Route path="select-flights" element={<SelectFlights />} />
-                <Route path="review-details" element={<ReviewDetails />} />
-              </Route>
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <AppRoutes />
       </QueryClientProvider>
     </HelmetProvider>
   );
---
+++ src/app/AppRoutes.jsx
@@ -0,0 +1,60 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ScrollToTop from '../components/ScrollToTop';
+
+// Layout
+import AppLayout from '../layout/AppLayout';
+import BookingLayout from '../layout/BookingLayout';
+
+// Landing Pages
+import Home from '../pages/landing-pages/Home';
+import FlightReservation from '../pages/landing-pages/FlightReservation';
+import DummyTicketForSchengenVisa from '../pages/landing-pages/DummyTicketForSchengenVisa';
+import OnwardTicket from '../pages/landing-pages/OnwardTicket';
+
+// Booking Pages
+import SelectFlights from '../pages/booking-pages/SelectFlights';
+import ReviewDetails from '../pages/booking-pages/ReviewDetails';
+import PaymentSuccess from '../pages/booking-pages/PaymentSuccess';
+
+// Legal Pages
+import TermsAndConditions from '../pages/legal-pages/TermsAndConditions';
+import PrivacyPolicy from '../pages/legal-pages/PrivacyPolicy';
+
+// FAQ
+import FAQ from '../pages/faq/FAQ';
+
+// Blog
+import BlogPost from '../pages/blog-pages/BlogPost';
+import Blog from '../pages/blog-pages/Blog';
+
+// Other Pages
+import Sitemap from '../pages/other/Sitemap';
+import PageNotFound from '../pages/other/PageNotFound';
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="dummy-ticket-schengen-visa" element={<DummyTicketForSchengenVisa />} />
+          <Route path="onward-ticket" element={<OnwardTicket />} />
+          <Route path="flight-reservation" element={<FlightReservation />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="sitemap" element={<Sitemap />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="payment-successful" element={<PaymentSuccess />} />
+          <Route path="terms-and-conditions" element={<TermsAndConditions />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="blog/:slug" element={<BlogPost />} />
+          <Route path="booking" element={<BookingLayout />}>
+            <Route path="select-flights" element={<SelectFlights />} />
+            <Route path="review-details" element={<ReviewDetails />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
