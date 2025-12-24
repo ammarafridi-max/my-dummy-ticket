2f14171f025ee10b2ad1f5aaf3951eeab0268387
Ammar Afridi <ammar.afridi95@gmail.com>
2025-12-24 14:10:29 +0400

    Content optimized

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -5,24 +5,57 @@ import PrimarySection from '../components/PrimarySection';
 import Container from '../components/Container';
 import PageTitle from '../components/PageTitle';
 import FAQAccordion from '../components/FAQAccordion';
+import Breadcrumb from '../components/Breadcrumb';
+
+const pageData = {
+  meta: {
+    title: 'Frequently Asked Questions',
+    description:
+      'Dummy tickets are flight reservations travelers use for various purposes, including visa applications. Book yours with My Dummy Ticket. Starting from AED 49.',
+    canonical: 'https://www.mydummyticket.ae/faq',
+  },
+  breadcrumb: [
+    { label: 'Home', path: '/' },
+    { label: 'FAQs', path: '/faq' },
+  ],
+  sections: {
+    hero: {
+      title: 'Frequently Asked Questions',
+      subtitle:
+        'Our FAQs section answers the most common questions about dummy tickets, including validity, usage, and verification. It’s designed to give you clear, quick, and reliable information so you can proceed with confidence.',
+    },
+  },
+};
 
 export default function FAQ() {
   return (
     <>
       <Helmet>
-        <title>Frequently Asked Questions</title>
+        <title>{pageData.meta.title}</title>
+        <link rel="canonical" href={pageData.meta.canonical} />
+        <meta name="robots" content="index, follow" />
+        <meta name="description" content={pageData.meta.description} />
       </Helmet>
-      <PrimarySection className="pt-10 pb-10">
-        <Container className="block md:grid md:grid-cols-[8fr_4fr]">
-          <div>
-            <PageTitle className="mb-10">Frequently Asked Questions</PageTitle>
+      <PrimarySection className="py-10 lg:py-15 bg-gray-50">
+        <Container>
+          <div className="flex flex-col lg:items-center lg:justify-center lg:text-center lg:max-w-200 lg:mx-auto">
+            <Breadcrumb paths={pageData?.breadcrumb} />
+            <PageTitle className="mt-3 mb-5">{pageData?.sections?.hero?.title}</PageTitle>
+            <p className="font-extralight text-sm lg:text-lg">
+              {pageData?.sections?.hero?.subtitle}
+            </p>
+          </div>
+        </Container>
+      </PrimarySection>
+      <PrimarySection className="py-10 lg:py-15 bg-white">
+        <Container>
+          <div className="flex flex-col lg:items-center lg:justify-center lg:max-w-240 lg:mx-auto gap-5">
             {formatFaqArray(faqArray, 'dummy ticket').map((faq, i) => (
-              <FAQAccordion key={i} question={faq.question}>
+              <FAQAccordion key={i} question={faq?.question}>
                 {faq.answer}
               </FAQAccordion>
             ))}
           </div>
-          <div></div>
         </Container>
       </PrimarySection>
     </>
