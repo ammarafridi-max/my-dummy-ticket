6865b3c0f5046de63ebcbb7f86d9c6412f4c5edc
Ammar Afridi <ammar.afridi95@gmail.com>
2025-12-30 16:50:39 +0400

    Create links for new dummy ticket SEO pages

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { pages } from './Navigation';
 import { HiOutlineXMark, HiOutlineBars3 } from 'react-icons/hi2';
+import { ChevronDown, ChevronRight } from 'lucide-react';
+import { useOutsideClick } from '../hooks/useOutsideClick';
 import Container from './Container';
 
 export default function MobileNavigation() {
+  const wrapperRef = useRef(null);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [openIndex, setOpenIndex] = useState(null);
+
+  useOutsideClick(wrapperRef, () => setMenuOpen(false));
 
   return (
     <nav className="block lg:hidden py-3 relative z-50">
@@ -32,21 +38,52 @@ export default function MobileNavigation() {
             className="fixed inset-0 bg-black opacity-60 z-40"
             onClick={() => setMenuOpen(false)}
           />
-          <div
-            className="fixed inset-0 z-50 flex items-start m-10 justify-center"
-            onClick={() => setMenuOpen(false)}
-          >
-            <div className="w-full h-fit bg-white shadow-md border border-gray-200 rounded-md">
-              {pages.map((page, i) => (
-                <a
-                  key={i}
-                  href={page.link}
-                  onClick={() => setMenuOpen(false)}
-                  className="block px-6 py-3 text-center text-base text-gray-800 hover:bg-gray-100 transition"
-                >
-                  {page.name}
-                </a>
-              ))}
+          <div className="fixed inset-0 z-50 flex items-start justify-start">
+            <div
+              ref={wrapperRef}
+              className="w-[80%] h-dvh bg-white shadow-md border border-gray-200"
+            >
+              {pages.map((page, i) => {
+                if (page.subpages) {
+                  return (
+                    <>
+                      <div
+                        key={i}
+                        className="flex items-center justify-between px-6 py-2 text-base font-light text-gray-800 hover:bg-gray-100 transition"
+                        onClick={() => setOpenIndex(openIndex === i ? null : i)}
+                      >
+                        <span>{page.name}</span>
+                        {openIndex === i ? <ChevronDown size={20} /> : <ChevronRight size={20} />}
+                      </div>
+
+                      {openIndex === i && page.subpages && (
+                        <div className="flex flex-col bg-gray-50">
+                          {page.subpages.map((sub, idx) => (
+                            <a
+                              key={idx}
+                              href={sub.link}
+                              className="px-10 py-2 font-light text-gray-700 hover:bg-gray-100"
+                            >
+                              {sub.name}
+                            </a>
+                          ))}
+                        </div>
+                      )}
+                    </>
+                  );
+                }
+
+                return (
+                  <a
+                    key={i}
+                    href={page.link}
+                    onClick={() => setMenuOpen(false)}
+                    className="flex items-center justify-between px-6 py-2 text-base font-light text-gray-800 hover:bg-gray-100 transition"
+                  >
+                    <span>{page.name}</span>
+                  </a>
+                );
+              })}
             </div>
           </div>
         </>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,31 @@
+import { ChevronDown } from 'lucide-react';
 import Container from './Container';
 
 export const pages = [
-  { name: 'Process', link: '/#process' },
-  { name: 'About', link: '/#about' },
-  { name: 'Benefits', link: '/#benefits' },
-  { name: 'FAQs', link: '/#faq' },
+  {
+    name: 'Dummy Tickets',
+    link: '/',
+    subpages: [
+      {
+        name: 'For Schengen Visa',
+        link: '/dummy-ticket-schengen-visa',
+      },
+      {
+        name: 'For US Visa',
+        link: '/dummy-ticket-us-visa',
+      },
+    ],
+  },
+  { name: 'Process', link: '#process' },
+  { name: 'About', link: '#about' },
+  { name: 'Benefits', link: '#benefits' },
+  { name: 'FAQs', link: '#faq' },
   { name: 'Email Us', link: 'mailto:info@mydummyticket.ae' },
 ];
 
 export default function Navigation() {
   return (
-    <header className="hidden lg:block bg-white/80 backdrop-blur-sm py-2 shadow-sm z-50">
+    <header className="hidden lg:block bg-white/80 backdrop-blur-sm py-0 shadow-sm z-50">
       <Container>
         <nav className="flex items-center justify-between py-2 font-outfit">
           <div className="w-[200px] flex-shrink-0">
@@ -24,16 +39,32 @@ export default function Navigation() {
             </a>
           </div>
 
-          <div className="flex items-center gap-7">
+          <div className="flex items-center gap-3">
             {pages.map((page, i) => (
-              <a
-                key={i}
-                href={page.link}
-                title={page.name}
-                className="text-[16px] font-light text-black/60 hover:text-black hover:font-normal capitalize transition-colors duration-300"
-              >
-                {page.name}
-              </a>
+              <div key={i} className="relative group">
+                <a
+                  href={page.link}
+                  title={page.name}
+                  className="flex items-center gap-1 text-[16px] font-normal text-black/50 hover:text-black py-4 px-2 capitalize transition-colors duration-300"
+                >
+                  <span>{page.name}</span>
+                  <span>{page.subpages ? <ChevronDown size={20} /> : ''}</span>
+                </a>
+
+                {page?.subpages && (
+                  <div className="hidden group-hover:flex flex-col w-70 absolute top-12 left-0 bg-white rounded-xl border border-gray-200 shadow-md overflow-hidden">
+                    {page.subpages.map((subpage, j) => (
+                      <a
+                        key={j}
+                        href={subpage.link}
+                        className="text-[15px] font-normal text-black/50 hover:text-black px-4 py-1.5 hover:bg-gray-100"
+                      >
+                        {subpage.name}
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </div>
             ))}
           </div>
         </nav>
