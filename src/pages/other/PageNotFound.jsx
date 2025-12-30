3656358fbbdafe1e7c9f64f4eaa9075fda014e09
Ammar Afridi <ammar.afridi95@gmail.com>
2025-12-30 16:53:16 +0400

    Title for 404 pages

--- src/pages/other/PageNotFound.jsx
+++ src/pages/other/PageNotFound.jsx
@@ -1,23 +1,30 @@
 import PrimaryButton from '../../components/PrimaryButton';
 import Container from '../../components/Container';
 import PrimarySection from '../../components/PrimarySection';
+import { Helmet } from 'react-helmet-async';
 
 export default function PageNotFound() {
   return (
-    <PrimarySection py="150px">
-      <Container>
-        <div className="text-center">
-          <h1 className="font-merriweather text-center text-primary-800 text-7xl font-bold">
-            404 Error!
-          </h1>
-          <h2 className="font-nunito text-center text-3xl font-normal">
-            The page you're looking for doesn't exist.
-          </h2>
-          <PrimaryButton mr="8px" href="/">
-            Go to Home
-          </PrimaryButton>
-        </div>
-      </Container>
-    </PrimarySection>
+    <>
+      <Helmet>
+        <title>Page Not Found!</title>
+        <meta name="robots" content="none" />
+      </Helmet>
+      <PrimarySection py="150px">
+        <Container>
+          <div className="text-center">
+            <h1 className="font-merriweather text-center text-primary-800 text-7xl font-bold">
+              404 Error!
+            </h1>
+            <h2 className="font-nunito text-center text-3xl font-normal">
+              The page you're looking for doesn't exist.
+            </h2>
+            <PrimaryButton mr="8px" href="/">
+              Go to Home
+            </PrimaryButton>
+          </div>
+        </Container>
+      </PrimarySection>
+    </>
   );
 }
