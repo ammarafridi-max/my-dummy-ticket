9eed15c2b41d16fef102dd0c740e97c92e96b789
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-08 09:20:41 +0400

    Add GA4 measurement id variable

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const BACKEND = import.meta.env.VITE_BACKEND_URL;
 export const VIEWTRIP_URL = import.meta.env.VITE_VIEWTRIP_BACKEND;
+export const GA4_MEASUREMENT_ID = import.meta.env.VITE_GA4_MEASUREMENT_ID;
