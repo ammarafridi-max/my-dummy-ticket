07b0545ce0aed6ece3723aa600b23bbe7f898831
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-10 14:09:58 +0400

    Prettier styling used

--- src/components/FormElements/SelectDate.jsx
+++ src/components/FormElements/SelectDate.jsx
@@ -2,22 +2,13 @@ import { useRef, useState, useEffect } from 'react';
 import Calendar from './Calendar';
 import InputWithIcon from './InputWithIcon';
 
-export default function SelectDate({
-  selectedDate,
-  onDateSelect,
-  minDate,
-  placeholder,
-  icon,
-}) {
+export default function SelectDate({ selectedDate, onDateSelect, minDate, placeholder, icon }) {
   const [showCalendar, setShowCalendar] = useState(false);
   const componentRef = useRef(null);
 
   useEffect(() => {
     function handleClickOutside(event) {
-      if (
-        componentRef.current &&
-        !componentRef.current.contains(event.target)
-      ) {
+      if (componentRef.current && !componentRef.current.contains(event.target)) {
         setShowCalendar(false);
       }
     }
@@ -40,13 +31,13 @@ export default function SelectDate({
       {showCalendar && (
         <div>
           <Calendar
-            onDateClick={(date) => {
+            onDateClick={date => {
               onDateSelect(date);
               setShowCalendar(false);
             }}
             showCalendar={showCalendar}
             setShowCalendar={setShowCalendar}
-            isDateDisabled={(date) => minDate && date < minDate}
+            isDateDisabled={date => minDate && date < minDate}
           />
         </div>
       )}
