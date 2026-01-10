d4aa4920c27126e49746116bac4c82d8cf7f90aa
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-10 14:08:38 +0400

    SegmentedRadioGroup created

---
+++ src/components/FormElements/SegmentedRadioGroup.jsx
@@ -0,0 +1,50 @@
+import { FaCheck } from 'react-icons/fa6';
+
+function SegmentedRadioGroup({ name, value, options, onChange, textAlign = 'left' }) {
+  return (
+    <div className="block lg:flex rounded-lg overflow-hidden border border-gray-300 divide-x divide-gray-200">
+      {options.map((option, index) => {
+        const isSelected = value === option.value;
+
+        return (
+          <label
+            key={index}
+            className={`relative flex flex-1 cursor-pointer transition-colors duration-200
+                ${
+                  isSelected
+                    ? 'bg-gray-100'
+                    : 'bg-white text-gray-800 hover:bg-gray-100 active:bg-gray-200'
+                }`}
+          >
+            <input
+              type="radio"
+              name={name}
+              value={option.value}
+              checked={isSelected}
+              onChange={() => onChange(option)}
+              className="absolute opacity-0"
+            />
+
+            <div
+              className={`w-full h-full p-3 lg:p-2.5 flex items-center justify-between text-${textAlign} gap-1 text-[15px] font-light border-2 border-transparent`}
+            >
+              <div>
+                <span>{option.label}</span>
+
+                {option.price !== undefined && (
+                  <>
+                    <span>-</span>
+                    <span className={`text-sm`}>AED {option.price} / person</span>
+                  </>
+                )}
+              </div>
+              <div>{isSelected && <FaCheck className="ml-1 text-primary-500" />}</div>
+            </div>
+          </label>
+        );
+      })}
+    </div>
+  );
+}
+
+export default SegmentedRadioGroup;
