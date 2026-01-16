dfb0fd86614e0652f47f1bdee9c8932a37c7948c
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-16 18:54:40 +0400

    Use Toaster from React Hot Toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { useEffect } from 'react';
 import { initializeGA } from './lib/analytics';
 import { HelmetProvider } from 'react-helmet-async';
-import { ToastContainer, Zoom } from 'react-toastify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { InsuranceProvider } from './context/InsuranceContext';
+import { Toaster } from 'react-hot-toast';
 import AppRoutes from './app/AppRoutes';
 
 const queryClient = new QueryClient({
@@ -21,11 +20,9 @@ function App() {
 
   return (
     <HelmetProvider>
-      <ToastContainer transition={Zoom} />
+      <Toaster />
       <QueryClientProvider client={queryClient}>
-        <InsuranceProvider>
-          <AppRoutes />
-        </InsuranceProvider>
+        <AppRoutes />
       </QueryClientProvider>
     </HelmetProvider>
   );
