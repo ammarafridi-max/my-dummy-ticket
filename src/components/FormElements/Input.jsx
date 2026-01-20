92417af65986d1744b74233108147b1130dfc54d
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-20 10:19:19 +0400

    Padding x update

--- src/components/FormElements/Input.jsx
+++ src/components/FormElements/Input.jsx
@@ -2,7 +2,7 @@ export default function Input({ icon, className = '', type = 'text', ...props })
   return (
     <input
       {...props}
-      className={`w-[100%] bg-white font-light text-[14.5px] py-2 px-4 outline-none placeholder:text-gray-400 placeholder:text-[14px] rounded-lg border border-gray-300 focus-visible:border-1.5 focus-visible:border-primary-500 ${className}`}
+      className={`w-[100%] bg-white font-light text-[14.5px] py-2 px-3 outline-none placeholder:text-gray-400 placeholder:text-[14px] rounded-lg border border-gray-300 focus-visible:border-1.5 focus-visible:border-primary-500 ${className}`}
     />
   );
 }
