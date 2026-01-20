a606a25689381c2089e582e014a3660bc15da910
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-20 10:13:13 +0400

    Add api for finalizing insurance

--- src/services/apiInsurance.js
+++ src/services/apiInsurance.js
@@ -2,10 +2,44 @@ import { apiFetch } from './apiClient';
 
 const URL = `/api/insurance`;
 
+export async function getNationalities() {
+  return await apiFetch(`${URL}/nationalities`);
+}
+
 export async function getQuotesApi({ journeyType, startDate, endDate, region, quantity }) {
   return await apiFetch(`${URL}/quote`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ journeyType, startDate, endDate, region, quantity }),
+  });
+}
+
+export async function finalizeInsuranceApi({
+  quoteId,
+  schemeId,
+  journeyType,
+  startDate,
+  endDate,
+  region,
+  quantity,
+  passengers,
+  email,
+  mobile,
+}) {
+  return await apiFetch(`${URL}/finalize`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      quoteId,
+      schemeId,
+      journeyType,
+      startDate,
+      endDate,
+      region,
+      quantity,
+      passengers,
+      email,
+      mobile,
+    }),
   });
 }
