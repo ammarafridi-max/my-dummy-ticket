f4ce8fc2ae816ca679388b4b32de47c91a98b068
Ammar Afridi <ammar.afridi95@gmail.com>
2026-01-20 10:54:48 +0400

    Wrap pages in TicketContext

--- src/app/AppRoutes.jsx
+++ src/app/AppRoutes.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { InsuranceProvider } from '../context/InsuranceContext';
+import { TicketProvider } from '../context/TicketContext';
 import ScrollToTop from '../components/ScrollToTop';
 
 // Layout
@@ -45,38 +46,40 @@ import PageNotFound from '../pages/other/PageNotFound';
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <InsuranceProvider>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<Home />} />
-            <Route path="dummy-ticket-schengen-visa" element={<DummyTicketForSchengenVisa />} />
-            <Route path="dummy-ticket-us-visa" element={<DummyTicketForUSVisa />} />
-            <Route path="emirates-dummy-ticket" element={<EmiratesDummyTicket />} />
-            <Route path="etihad-dummy-ticket" element={<EtihadDummyTicket />} />
-            <Route path="onward-ticket" element={<OnwardTicket />} />
-            <Route path="flight-reservation" element={<FlightReservation />} />
-            <Route path="travel-insurance" element={<TravelInsurance />} />
-            <Route path="*" element={<PageNotFound />} />
-            <Route path="sitemap" element={<Sitemap />} />
-            <Route path="faq" element={<FAQ />} />
-            <Route path="payment-successful" element={<PaymentSuccess />} />
-            <Route path="terms-and-conditions" element={<TermsAndConditions />} />
-            <Route path="privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="blog" element={<Blog />} />
-            <Route path="blog/:slug" element={<BlogPost />} />
-            <Route path="booking" element={<BookingLayout />}>
-              <Route path="select-flights" element={<SelectFlights />} />
-              <Route path="review-details" element={<ReviewDetails />} />
+      <TicketProvider>
+        <InsuranceProvider>
+          <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<Home />} />
+              <Route path="dummy-ticket-schengen-visa" element={<DummyTicketForSchengenVisa />} />
+              <Route path="dummy-ticket-us-visa" element={<DummyTicketForUSVisa />} />
+              <Route path="emirates-dummy-ticket" element={<EmiratesDummyTicket />} />
+              <Route path="etihad-dummy-ticket" element={<EtihadDummyTicket />} />
+              <Route path="onward-ticket" element={<OnwardTicket />} />
+              <Route path="flight-reservation" element={<FlightReservation />} />
+              <Route path="travel-insurance" element={<TravelInsurance />} />
+              <Route path="*" element={<PageNotFound />} />
+              <Route path="sitemap" element={<Sitemap />} />
+              <Route path="faq" element={<FAQ />} />
+              <Route path="payment-successful" element={<PaymentSuccess />} />
+              <Route path="terms-and-conditions" element={<TermsAndConditions />} />
+              <Route path="privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="blog" element={<Blog />} />
+              <Route path="blog/:slug" element={<BlogPost />} />
+              <Route path="booking" element={<BookingLayout />}>
+                <Route path="select-flights" element={<SelectFlights />} />
+                <Route path="review-details" element={<ReviewDetails />} />
+              </Route>
+              <Route element={<InsuranceBookingLayout />}>
+                <Route path="travel-insurance/quotes" element={<Quotes />} />
+                <Route path="travel-insurance/passenger-details" element={<PassengerDetails />} />
+                <Route path="travel-insurance/payment" element={<InsurancePayment />} />
+              </Route>
             </Route>
-            <Route element={<InsuranceBookingLayout />}>
-              <Route path="travel-insurance/quotes" element={<Quotes />} />
-              <Route path="travel-insurance/passenger-details" element={<PassengerDetails />} />
-              <Route path="travel-insurance/payment" element={<InsurancePayment />} />
-            </Route>
-          </Route>
-        </Routes>
-      </InsuranceProvider>
+          </Routes>
+        </InsuranceProvider>
+      </TicketProvider>
     </BrowserRouter>
   );
 }
